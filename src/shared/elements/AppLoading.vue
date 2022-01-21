04a3e3f7a59b5d039fdd71a30597f8249f698a33
Lian Robin Castillo <castillolianrobin@gmail.com>
2022-01-21 20:01:45 +0800

    Add AppLoading component (untested)

---
+++ src/shared/elements/AppLoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <loading active loader="bars"  color="white" opacity="0" :is-full-page="false" v-bind="$attrs"></loading>
+</template>
+
+<script>
+import Loading from 'vue-loading-overlay';
+export default {
+  components: {
+    Loading,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+// @import url();
+</style>
