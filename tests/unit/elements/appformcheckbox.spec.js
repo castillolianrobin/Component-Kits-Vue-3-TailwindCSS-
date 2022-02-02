f2500db7668d0cf921c4c0825747c523bfb492ce
Lian Robin Castillo <castillolianrobin@gmail.com>
2022-02-02 13:29:32 +0800

    Update testcase for AppFormCheckbox

--- tests/unit/elements/appformcheckbox.spec.js
+++ tests/unit/elements/appformcheckbox.spec.js
@@ -1,21 +1,20 @@
-// import { AppFormCheckBox } from '@/shared/elements/';
-import AppFormCheckBox from "@/shared/elements/AppFormCheckBox.vue";
+import AppFormCheckbox from "@/shared/elements/AppFormCheckbox.vue";
 import { inputTest } from "../common/input.spec";
 import { namedSlotTest } from "../common/slot.spec";
 import { validationTest } from "../common/validation.spec";
 
-describe("AppFormCheckBox.vue", () => {
+describe("AppFormCheckbox.vue", () => {
   // Slot Test
-  namedSlotTest(AppFormCheckBox, "inactive");
-  namedSlotTest(AppFormCheckBox, "active", { props: { modelValue: true } });
+  namedSlotTest(AppFormCheckbox, "inactive");
+  namedSlotTest(AppFormCheckbox, "active", { props: { modelValue: true } });
   // active slot with multiple checkbox (item)
   const checkboxItems = [{ value: 'val1', label: 'Test val 1' }, { value: 'val2', label: 'val2' }];
   const selectedCheckbox = checkboxItems[1];
-  namedSlotTest(AppFormCheckBox, "active", { props: { modelValue: [selectedCheckbox.value], items: checkboxItems, } });
+  namedSlotTest(AppFormCheckbox, "active", { props: { modelValue: [selectedCheckbox.value], items: checkboxItems, } });
 
   // Input related Test
-  inputTest(AppFormCheckBox, "AppFormCheckBox.vue");
+  inputTest(AppFormCheckbox, "AppFormCheckbox.vue");
 
   // validation related Test
-  validationTest(AppFormCheckBox, "AppFormCheckBox.vue");
+  validationTest(AppFormCheckbox, "AppFormCheckbox.vue");
 });
